545fd9c1b11b6bd006438d9486436cae05d27137
Sol <johndoe@example.com>
2015-07-24 15:45:38 +0000

    add data w forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,19 @@ class ApplicationController < ActionController::Base
   
 def show
   @user = User.find_by_id(params["id"])
-  render "show"
+end
+
+def new 
 end
   
+  def create
+    u = User.new
+    u.name=params["name"]
+    u.join_date=params["date"]
+    u.descrip=params["descrip"]
+    u.image=params["image"]
+    u.location=params["loc"]
+    u.save
+    redirect_to "/user/#{ u.id }"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/user/:id" => "application#show"
+  get "/new_user" => "application#new"
+  get "/create_user" => "application#create"
 end
